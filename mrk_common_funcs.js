ad14a98d3363cf7d7a4ca301ff9592a26c5f725f
MrKraken Office <main@mrkraken.com>
2021-01-11 11:32:50 +0000

    Update mrk_common_funcs.js
    Naughty var slipped in from a very old function

--- mrk_common_funcs.js
+++ mrk_common_funcs.js
@@ -48,10 +48,10 @@ module.exports.timeDiff = function(a,b)
     return timeDiff; //return time difference in miliseconds
     
 }
-
+/** Miliseconds to Minutes & Seconds */
  module.exports.msToMandS = function(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    let minutes = Math.floor(millis / 60000);
+    let seconds = ((millis % 60000) / 1000).toFixed(0);
     //ES6 interpolated literals/template literals 
     //If seconds is less than 10 put a zero in front.
     return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
